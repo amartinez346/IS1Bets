abd996c366fd1d9bc56d040027b345515846647f
Aitor <amartinez346@ikasle.ehu.eus>
2019-03-12 13:16:06 +0100

    doxygen

---
+++ html/classdomain_1_1User.js
@@ -0,0 +1,19 @@
+var classdomain_1_1User =
+[
+    [ "User", "classdomain_1_1User.html#a6a19ce3695034e587026e08628e4a94b", null ],
+    [ "getContraseña", "classdomain_1_1User.html#a44968d733c04b3959f66402ced33984b", null ],
+    [ "getDNI", "classdomain_1_1User.html#a9a0a7affd1ab9ba7d882317d5da47888", null ],
+    [ "getSaldo", "classdomain_1_1User.html#ab3b58a7e3f345ddf8df5b87ad772b7d5", null ],
+    [ "getTipo", "classdomain_1_1User.html#a38873ecd67b4dccdf3bb067df7d9eb06", null ],
+    [ "getUser", "classdomain_1_1User.html#a1d9197078569283ae7410f9af2d8d013", null ],
+    [ "setContraseña", "classdomain_1_1User.html#accfe716a748fb978adda658811f325d3", null ],
+    [ "setDNI", "classdomain_1_1User.html#acdb5b5bf6b571a77af5596da0da953fa", null ],
+    [ "setSaldo", "classdomain_1_1User.html#ab0da73a44b69fbb13668ec2093e8d3e7", null ],
+    [ "setTipo", "classdomain_1_1User.html#aa5010bb10a4aaacd7731ca313e2ae6ce", null ],
+    [ "setUser", "classdomain_1_1User.html#ae58cb8a12b63ea9f60b8308ab3e09712", null ],
+    [ "contraseña", "classdomain_1_1User.html#abf1ae1b46994864b6f23e1965b5f0cd0", null ],
+    [ "DNI", "classdomain_1_1User.html#a9f89fcc3016d3a74f4660f1e3c133882", null ],
+    [ "saldo", "classdomain_1_1User.html#abfe10a19b15929745bc34610db010938", null ],
+    [ "tipo", "classdomain_1_1User.html#a3e3d2bcaf19448f31142d301db0493be", null ],
+    [ "usuario", "classdomain_1_1User.html#a3039c018dafefc6ce963346e58f80fe6", null ]
+];
---
+++ html/search/functions_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['login',['Login',['../interfacebusinessLogic_1_1BLFacade.html#a51f084d973c58559ceb611ef92aed3f3',1,'businessLogic.BLFacade.Login()'],['../classbusinessLogic_1_1BLFacadeImplementation.html#abc11dd684812d75b937e9a74dce274d6',1,'businessLogic.BLFacadeImplementation.Login()']]]
+];
